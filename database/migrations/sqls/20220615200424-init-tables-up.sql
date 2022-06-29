33fcb9c7977fad956e7f453b6d21183ca081a33e
marshblocker <glmarinas@up.edu.ph>
2022-06-29 20:19:35 +0800

    Add new entries in the database

--- database/migrations/sqls/20220615200424-init-tables-up.sql
+++ database/migrations/sqls/20220615200424-init-tables-up.sql
@@ -13,7 +13,9 @@ CREATE TABLE `users` (
 INSERT INTO `users`
 VALUES ('WG-g52obz', 'grep', '3A31C82929DB391B0E0BD7424E0B4275E6EB1BD6', 'grep@gmail.com', 'user', 1),
        ('ri7dUmhoe', 'optimus', '1F1D3772887289D67B355B0CB11AD98554ACBF5F', 'optimus@gmail.com', 'admin', 1),
-       ('dNyUCv4an', 'prime', 'EA756C3AD19A39CFCC25922FF018BA8F797C88C3', 'prime@gmail.com', 'superadmin', 1);
+       ('dNyUCv4an', 'alien', 'F9965239005F50B3EEE8D3C472B9ECF014EA8588', 'alien@gmail.com', 'user', 1),
+       ('HmPoFyj3y', 'cloud', '000E793DB70C59309FA6F0F36D0046D110F3BE3C', 'cloud@gmail.com', 'user', 1),
+       ('7toCz2QqA', 'prime', 'EA756C3AD19A39CFCC25922FF018BA8F797C88C3', 'prime@gmail.com', 'superadmin', 1);
 
 CREATE TABLE `pages` (
  `page_id` CHAR(9) NOT NULL UNIQUE,
@@ -25,4 +27,7 @@ CREATE TABLE `pages` (
 
 INSERT INTO `pages`
 VALUES ('bAtxxvMLY', 'Philippines', 'Country in South-East Asia.', 0),
-       ('K5ktjgIEi', 'Gun', 'To shoot stuff.', '1');
+       ('K5ktjgIEi', 'Gun', 'To shoot stuff.', '0'),
+       ('iW0M66d6V', 'Star', 'Made of gas.', '1'),
+       ('Mhjsq3Col', 'Boomerang', 'It returns!', '1'),
+       ('ndOI20J82', 'Zebra', 'A black-striped animal.', '1');
