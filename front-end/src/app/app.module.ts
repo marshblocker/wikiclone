f0c213bfdb29f5290fba240ff9a691e93d539742
marshblocker <glmarinas@up.edu.ph>
2022-06-30 19:20:54 +0800

    Initial front-end components

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleBodyComponent } from './components/article-body/article-body.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleBodyComponent,
+    FooterComponent,
+    NavBarComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
