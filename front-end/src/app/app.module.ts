952a3152806ce4999561d2c7a687b23d447a4cd4
marshblocker <glmarinas@up.edu.ph>
2022-07-03 14:40:15 +0800

    Initialize LoginComponent

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PageService } from './services/page.service';
 import { HomeComponent } from './components/home/home.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { RandomPageComponent } from './components/random-page/random-page.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
