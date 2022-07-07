a7e65d4c5c8b8a3f36c1f0210f5bfd77b3cb8dd1
marshblocker <glmarinas@up.edu.ph>
2022-07-07 11:29:22 +0800

    Call controllers in route of login and users API

--- back-end/routes/login.js
+++ back-end/routes/login.js
@@ -1,9 +1,11 @@
 var express = require('express');
 const LoginController = require('../controllers/login/LoginController');
-const userController = require('../controllers/user/userController');
+const UserReadController = require('../controllers/user/UserReadController');
+const UserDAO = require('../DAO/UserDAO');
+const userReadController = new UserReadController(new UserDAO());
 var router = express.Router();
 
-const loginController = new LoginController(userController.userReadController);
+const loginController = new LoginController(userReadController);
 
 router.post('/', async (req, res) => await loginController.loginUser(req, res));
 
--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -1,6 +1,18 @@
 var express = require('express');
 const utils = require('../utils');
 
+const UserDAO = require('../DAO/UserDAO');
+const UserCreateController = require('../controllers/user/UserCreateController');
+const UserReadController = require('../controllers/user/UserReadController');
+const UserUpdateController = require('../controllers/user/UserUpdateController');
+const UserDeleteController = require('../controllers/user/UserDeleteController');
+
+const userDAO = new UserDAO();
+const userCreateController = new UserCreateController(userDAO);
+const userReadController = new UserReadController(userDAO);
+const userUpdateController = new UserUpdateController(userDAO);
+const userDeleteController = new UserDeleteController(userDAO);
+
 var router = express.Router();
 
 router.post('/', async (req, res) => await userCreateController.createUser(req, res));
