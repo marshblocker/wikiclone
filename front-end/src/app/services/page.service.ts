32f4a2c0068858c9ed02fecc13af720f3ee0e2d3
marshblocker <glmarinas@up.edu.ph>
2022-07-12 11:25:55 +0800

    Implement new GET methods in PageService

--- front-end/src/app/services/page.service.ts
+++ front-end/src/app/services/page.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
-import { PageContent, Page } from '../interfaces/page.interface';
+import { PageContent, Page, PageSearchResultView } from '../interfaces/page.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +9,7 @@ export class PageService {
 
   constructor(private http: HttpClient) { }
 
-  public getPageContent(pageId: string): Promise<PageContent> {
+  public getPage(pageId: string): Promise<Page> {
     return new Promise((resolve, reject) => {
       const url = 'http://localhost:3000/pages/' + pageId;
       this.http.get<Page>(
@@ -23,7 +23,46 @@ export class PageService {
         if (pageResponse.body === null) {
           return reject('Page is null!');
         }
-        return resolve(pageResponse.body.content);
+        return resolve(pageResponse.body);
+      });
+    });
+  }
+
+  public getPageByTitle(title: string): Promise<Page> {
+    return new Promise((resolve, reject) => {
+      const url = 'http://localhost:3000/pages?title=' + title;
+      this.http.get<Page>(
+        url, 
+        { 
+          headers: { 'Authorization': document.cookie }, 
+          observe: 'response', 
+          responseType: 'json' 
+        }
+      ).subscribe((pageResponse: HttpResponse<Page>) => {
+        console.log(pageResponse);
+        if (pageResponse.body === null) {
+          return reject('Page is null!');
+        }
+        return resolve(pageResponse.body);
+      });
+    });
+  }
+
+  public getAllPages(): Promise<Page[]> {
+    return new Promise((resolve, reject) => {
+      const url = 'http://localhost:3000/pages';
+      this.http.get<Page[]>(
+        url, 
+        { 
+          headers: { 'Authorization': document.cookie }, 
+          observe: 'response', 
+          responseType: 'json' 
+        }
+      ).subscribe((allPagesResponse: HttpResponse<Page[]>) => {
+        if (allPagesResponse.body === null) {
+          return reject('Get all page id request failed!');
+        }
+        return resolve(allPagesResponse.body);
       });
     });
   }
@@ -51,6 +90,29 @@ export class PageService {
     });
   }
 
+  public getAllPageTitles(): Promise<string[]> {
+    return new Promise((resolve, reject) => {
+      const url = 'http://localhost:3000/pages';
+      this.http.get<Page[]>(
+        url, 
+        { 
+          headers: { 'Authorization': document.cookie }, 
+          observe: 'response', 
+          responseType: 'json' 
+        }
+      ).subscribe((allPagesResponse: HttpResponse<Page[]>) => {
+        let allPageTitles: string[] = [];
+        if (allPagesResponse.body === null) {
+          return reject('Get all page id request failed!');
+        }
+        allPagesResponse.body.forEach(page => {
+          allPageTitles.push(page.content.title);
+        })
+        return resolve(allPageTitles);
+      });
+    });
+  }
+
   public getPagesBasedOnSearchString(searchString: string): Promise<PageSearchResultView[]> {
     return new Promise((resolve, reject) => {
       const url = 'http://localhost:3000/pages?contains=' + searchString;
@@ -70,6 +132,7 @@ export class PageService {
     });
   }
 
+  public submitNewPage(content: PageContent): Promise<Page> {
     return new Promise((resolve, reject) => {
       const url = 'http://localhost:3000/pages';
       this.http.post<Page>(url, { 'content': content }, 
@@ -82,12 +145,12 @@ export class PageService {
           if (newPageResponse.body === null) {
             return reject('New page is null!');
           }
-          return resolve(newPageResponse.body.page_id);
+          return resolve(newPageResponse.body);
         });
     })
   }
 
-  public updatePage(pageId: string, content: PageContent): Promise<PageContent> {
+  public updatePage(pageId: string, content: PageContent): Promise<Page> {
     return new Promise((resolve, reject) => {
       const url = 'http://localhost:3000/pages/' + pageId + '/content';
       this.http.patch<Page>(
@@ -98,9 +161,14 @@ export class PageService {
           responseType: 'json'
         }
       ).subscribe((updatedPageResponse: HttpResponse<Page>) => {
-        if (updatedPageResponse.body === null) {
+        if (updatedPageResponse.body == null) {
           return reject('Updated page is null!');
         }
+        return resolve(updatedPageResponse.body);
+      })
+    });
+  }
+
         return resolve(updatedPageResponse.body.content);
       })
     });
