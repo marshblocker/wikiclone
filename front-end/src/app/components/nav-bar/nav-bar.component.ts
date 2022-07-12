639ebd04c8384bb895f6810c8d1c2adadf013d18
marshblocker <glmarinas@up.edu.ph>
2022-07-12 11:17:33 +0800

    Add link to admin dashboard and article history in navbar

--- front-end/src/app/components/nav-bar/nav-bar.component.ts
+++ front-end/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserPublic } from 'src/app/interfaces/user.interface';
 
 @Component({
   selector: 'app-nav-bar',
@@ -10,10 +11,15 @@ export class NavBarComponent implements OnInit {
   @Input() pageType = 'Article';
   @Input() currentRoute!: string;
   @Input() currentUser?: string;
-  @Input() currentUserId?: string;
+  @Input() currentUsername?: string;
+  @Input() currentRole?: string;
+  @Input() currentCanEdit?: boolean;
+  @Input() freezePage?: boolean;
   @Input() loggedIn = false;
   @Input() viewingArticle = false;
   @Output() onLogout = new EventEmitter();
+
+  hideArticleEditButton = true;
 
   constructor(private router: Router) { }
 
@@ -28,5 +34,7 @@ export class NavBarComponent implements OnInit {
     const searchString = (<HTMLInputElement>document.getElementById('search-bar')).value;
     this.router.navigateByUrl('/wiki/search?search-string=' + searchString);
   }
+
+  
 
 }
