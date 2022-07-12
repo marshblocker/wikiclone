fb85a7c91ea853aad05776656af02d9c0543aa96
marshblocker <glmarinas@up.edu.ph>
2022-07-12 11:29:43 +0800

    The 'Create New Article' link in the side bar is only available when the user is logged in and can edit.

--- front-end/src/app/components/side-bar/side-bar.component.ts
+++ front-end/src/app/components/side-bar/side-bar.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class SideBarComponent implements OnInit {
   @Input() loggedIn = false;
+  @Input() canEdit? = false;
 
   constructor() { }
 
